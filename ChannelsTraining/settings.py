5eee837d2a6dc1841c89b9c4dd0b49930728e5d3
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-13 19:08:56 +0300

    Установлен channels и добавлено приложение chat

--- ChannelsTraining/settings.py
+++ ChannelsTraining/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -69,7 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ChannelsTraining.wsgi.application'
-
+ASGI_APPLICATION = 'ChannelsTraining.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
