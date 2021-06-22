779c691942557250bfd52046104c70a99b7a103a
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-22 14:59:01 +0300

    Правка asgi.py

--- ChannelsTraining/asgi.py
+++ ChannelsTraining/asgi.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+import django
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
